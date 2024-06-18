9506c9a0e4a29cdbbaf58482f0b05e3148755bfb
Marcellinus Meyer <marcellinus@meyer.ch>
2024-06-18 10:44:47 +0200

    wip

--- src/pinger.js
+++ src/pinger.js
@@ -1,73 +1,71 @@
 const address = document.getElementById("address");
 const output = document.getElementById("output");
 const result = document.getElementById("result");
-const button = document.getElementById("ping");
 
-function ping_run() {
-    /* global cockpit */
-    cockpit.spawn(["ping", "-c", "4", address.value])
-            .stream(ping_output)
-            .then(ping_success)
-            .catch(ping_fail);
-
-    result.textContent = "";
-    output.textContent = "";
-}
-
-function ping_success() {
+function spawn_success() {
     result.style.color = "green";
     result.textContent = "success";
 }
 
-function ping_fail() {
+function spawn_fail() {
     result.style.color = "red";
     result.textContent = "fail";
 }
 
-function ping_output(data) {
+function spawn_output(data) {
     output.append(document.createTextNode(data));
 }
 
-function whoami_run() {
-    /* global cockpit */
-    cockpit.spawn(["whoami"])
-            .stream(ping_output)
-            .then(ping_success)
-            .catch(ping_fail);
-
-    result.textContent = "";
-    output.textContent = "";
-}
-
 function storcli_run(cmd) {
-    /* global cockpit */
     cockpit.spawn(["/opt/MegaRAID/storcli/storcli64", cmd], { environ: ["LC_ALL=C"], superuser: "require" })
-            .stream(ping_output)
-            .then(ping_success)
-            .catch(ping_fail);
+            .stream(spawn_output)
+            .then(spawn_success)
+            .catch(spawn_fail);
 
     result.textContent = "";
     output.textContent = "";
 }
 function lsscsi_run(cmd) {
-    /* global cockpit */
     cockpit.spawn(["lsscsi", cmd], { environ: ["LC_ALL=C"], superuser: "require" })
-            .stream(ping_output)
-            .then(ping_success)
-            .catch(ping_fail);
+            .stream(spawn_output)
+            .then(spawn_success)
+            .catch(spawn_fail);
+
+    result.textContent = "";
+    output.textContent = "";
+}
+function lsscsi_run(cmd) {
+    cockpit.spawn(["lsscsi", cmd], { environ: ["LC_ALL=C"], superuser: "require" })
+            .stream(spawn_output)
+            .then(spawn_success)
+            .catch(spawn_fail);
+
+    result.textContent = "";
+    output.textContent = "";
+}
+function lsscsi_run(cmd) {
+    cockpit.spawn(["lsscsi", cmd], { environ: ["LC_ALL=C"], superuser: "require" })
+            .stream(spawn_output)
+            .then(spawn_success)
+            .catch(spawn_fail);
+
+    result.textContent = "";
+    output.textContent = "";
+}
+function sg_ses_run(cmd) {
+    cockpit.spawn(["sg_ses", cmd], { environ: ["LC_ALL=C"], superuser: "require" })
+            .stream(spawn_output)
+            .then(spawn_success)
+            .catch(spawn_fail);
 
     result.textContent = "";
     output.textContent = "";
 }
 
-
-// Connect the button to starting the "ping" process
-// button.addEventListener("click", whoami_run);
-
-// Send a 'init' message.  This tells integration tests that we are ready to go
 cockpit.transport.wait(function() {
-    whoami_run(); 
     storcli_run("show");
+    storcli_run("/c0 show");
+    storcli_run("/c1 /e0 show");
+    sg_ses_run("sg8 --join");
     lsscsi_run("-g");
-
 });
